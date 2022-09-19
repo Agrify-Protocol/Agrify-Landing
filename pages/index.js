d50ad152f8634969b1deee2c4f1311abd61f73ae
Samuel Adeyemi <Samuel3adeyemi@live.com>
2022-09-20 01:10:04 +1000

    sams part done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import InvestmentSection from "../components/section";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className='bg-gray-100'>
+      <div className='p-5 lg:p-10'>
+        <InvestmentSection />
+      </div>
+      <Footer />
+    </div>
+  );
 }
